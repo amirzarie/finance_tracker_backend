4b9d14e698e60fbac8a8cb6980152573a42c9ec6
Amir Zarie <40341663+amirzarie@users.noreply.github.com>
2023-05-29 10:35:53 -0400

    Started the project from scratch

--- main.py
+++ main.py
@@ -1,57 +1,7 @@
-# run the following code in the terminal to start your app (on default port 8000): uvicorn main:app --reload
-# On the web app, navigate to localhost:8000/docs to send requests and test out your paths.
-
-from fastapi import FastAPI, Query
-from pydantic import BaseModel
-from typing import Optional
+from fastapi import FastAPI
 
 app = FastAPI()
-
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
-
-
-@app.post("/")
-async def post():
-    return {"message": "Hello from the post route"}
-
-
-@app.get("/items/{item_id}")
-async def get_item(item_id: int):
-    return {"item_id": item_id}
-
-
-fake_items_dv = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
-
-
-@app.get("/item/{item_id}")
-async def get_item(item_id: int, q: str = None):
-    if q:
-        return {"item_id": item_id, "q": q}
-    return {"item_id": item_id}
-
-
-class Item(BaseModel):
-    name: str
-    description: Optional[str] = None
-    price: float
-    tax: Optional[float] = None
-
-
-@app.post("/item")
-async def create_item(item: Item):
-    item_dict = item.dict()
-    if item.tax:
-        price_with_tax = item.price + item.tax
-        item_dict.update({"price_with_tax": price_with_tax})
-    return item_dict
-
-
-@app.get("/item")
-async def read_items(q: list[str] | None = Query(None, max_length=10)):
-    results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
-    if q:
-        results.update({"q": q})
-    return results
