f1bd9910d64befe1ef7ab63c378256c2aa9398b8
Amir Zarie <40341663+amirzarie@users.noreply.github.com>
2023-06-05 15:09:54 -0400

    Completed the psql/sqlalchemy database connection.

---
+++ db/database.py
@@ -0,0 +1,11 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:1234SQL420!@localhost/financetracker"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={}, future=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine, future=True)
+
+Base = declarative_base()
